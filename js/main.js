48e9a46cbe3e21ebc08d4c52e02050015b1b902c
StepanD <33865426+SteveBlackBird@users.noreply.github.com>
2018-08-10 16:38:18 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mainNav").style.width = "250px";
-    document.getElementById("mainContainer").style.marginLeft = "250px";
-}
-function closeNav() {
-    document.getElementById("mainNav").style.width = "0";
-    document.getElementById("mainContainer").style.marginLeft = "0";
-}
