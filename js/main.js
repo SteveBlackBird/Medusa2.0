1565a0f6d09cf81486224b4746d09b982da3ba87
StepanD <33865426+SteveBlackBird@users.noreply.github.com>
2018-08-10 14:06:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mainNav").style.width = "250px";
+    document.getElementById("mainContainer").style.marginLeft = "250px";
+}
+function closeNav() {
+    document.getElementById("mainNav").style.width = "0";
+    document.getElementById("mainContainer").style.marginLeft = "0";
+}
