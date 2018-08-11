a3f51a53e84691eb2dade3ed167a7e04f021ec58
StepanD <33865426+SteveBlackBird@users.noreply.github.com>
2018-08-12 00:41:34 +0300

    Update gallery.js
    Add lightbox js

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,36 @@
-<tr>
-	<td class="prof">
-	</td>
-	<td class="time">
-		от 64 ч
-	</td>
-</tr>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+var gallM = document.getElementById('galleryModal');
+
+function openModal() {
+	gallM.style.display = "block";
+}
+
+function closeGalModal() {
+	gallM.style.display = "none";
+}
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName('mySlides');
+	var captionText = document.getElementById('caption');
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex-1].style.display = "block";
+}
